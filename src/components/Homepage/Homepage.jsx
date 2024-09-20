f39b2a55744dad400147d6f5f948ac332eaa1bbf
HitG010 <hiteshgupta_23ec091@dtu.ac.in>
2024-09-20 18:52:17 +0530

    ChatBot Button

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { getUserDetails } from '../../APIs/userDetails';
+import React from "react";
+import { useState, useEffect } from "react";
+import { getUserDetails } from "../../APIs/userDetails";
 
 function Home() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            const userDetails = await getUserDetails();
-            if(userDetails) setUser(userDetails);
-        };
-        fetchUserDetails();
-    }, []);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const userDetails = await getUserDetails();
+      if (userDetails) setUser(userDetails);
+    };
+    fetchUserDetails();
+  }, []);
 
-    if(!user) return <div>Loading...</div>;
+  if (!user) return <div>Loading...</div>;
 
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
-      <div className='text-3xl'>Hello {user.username} 👋</div>
-      <div className='text-6xl font-bold'>This is the homepage</div>
-      <button onClick={() => {
-        window.location.href = '/upload-video'
-      }} className='px-4 py-2 bg-[#f1f3f5] text-[#1e1e1e] rounded-full font-semibold text-xl mt-6'>Test A Video</button>
-      <button onClick={() => {
-        window.location.href = 'http://localhost:5000/logout'
-      }} className='px-4 py-2 bg-[#252525] text-[#f1f3f5] rounded-full font-semibold text-xl mt-6'>Logout</button>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div className="text-3xl">Hello {user.username} 👋</div>
+      <div className="text-6xl font-bold">This is the homepage</div>
+      <button
+        onClick={() => {
+          window.location.href = "/upload-video";
+        }}
+        className="px-4 py-2 bg-[#f1f3f5] text-[#1e1e1e] rounded-full font-semibold text-xl mt-6"
+      >
+        Test A Video
+      </button>
+      <button
+        onClick={() => {
+          window.location.href =
+            "https://0x46527403138b59e6ca8aba0bd37c77831f6a65b6.us.gaianet.network";
+        }}
+        className="px-4 py-2 bg-[#f1f3f5] text-[#1e1e1e] rounded-full font-semibold text-xl mt-6"
+      >
+        Chatbot
+      </button>
+      <button
+        onClick={() => {
+          window.location.href = "http://localhost:5000/logout";
+        }}
+        className="px-4 py-2 bg-[#252525] text-[#f1f3f5] rounded-full font-semibold text-xl mt-6"
+      >
+        Logout
+      </button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
