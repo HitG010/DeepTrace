b5c5eb64366fcd7312946c3d84af4db6d9425043
Kartik <kartikbindra_23cs211@dtu.ac.in>
2024-09-21 09:53:06 +0530

    change contract address

--- Smart Contracts/scripts/deploy.js
+++ Smart Contracts/scripts/deploy.js
@@ -13,4 +13,4 @@ main().catch((error) => {
   process.exitCode = 1;
 });
 
-//0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
--- src/contractDeets.jsx
+++ src/contractDeets.jsx
@@ -18,7 +18,7 @@ async function loadProvider() {
     account = await signer.getAddress();
     
     // Load contract
-    const contractAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
+    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
     const newContract = new ethers.Contract(contractAddress, VideoStorage.abi, signer);
     contractGlobal = newContract;
     
